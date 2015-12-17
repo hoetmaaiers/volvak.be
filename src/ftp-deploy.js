325996d18149ce6b2ad2c1383d92709b58344ac4
Robin Houdmeyers <houdmeyers@gmail.com>
2015-12-17 19:02:34 +0100

    add deploy script

---
+++ src/ftp-deploy.js
@@ -0,0 +1,15 @@
+var fs = require( 'vinyl-fs' ),
+    ftp = require( 'vinyl-ftp' );
+
+var conn = new ftp({
+  host:     'users.telenet.be',
+  user:     'w225685',
+  password: 'xxxxxxx',
+  log:      console.log
+});
+
+var globs = ['./build/**/*'];
+
+fs.src( globs )
+  .pipe( conn.newerOrDifferentSize('.'))
+  .pipe( conn.dest('.'));
